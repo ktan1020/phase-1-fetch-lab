41adbd747b29f8e278da1becbf38fa2c03725813
Keely Tan <keely.tan94@gmail.com>
2025-08-17 14:30:01 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json()) // convert response to JSON
+    .then(books => renderBooks(books)); // pass JSON data to renderBooks
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector("main");
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    const h2 = document.createElement("h2");
+    h2.textContent = book.name; // book title
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+// call fetchBooks when the page loads
+document.addEventListener("DOMContentLoaded", fetchBooks);
